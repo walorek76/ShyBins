4ac100cf1f539ec182681e643ade02f3cbcda694
walorek76 <karollehrich@gmail.com>
2025-09-27 14:54:13 +0200

    add audioPause

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _gamePauseCanvas;
+    [SerializeField] private GameObject _audioSource;
+    public static GameManager instance;
+    private bool activePause = false;
+    private bool activeAudio = true;
     private void Start()
     {
         _gamePauseCanvas.SetActive(false);
+        _audioSource.GetComponent<AudioSource>().volume = 0.1f;
     }
-    public static GameManager instance;
-    private bool activePause = false;
-    [SerializeField] private GameObject _gamePauseCanvas;
-    
+
     private void Awake()
     {
         if (instance == null)
@@ -26,17 +31,17 @@ public class GameManager : MonoBehaviour
     public void PauseGame()
     {
         if (activePause)
-            {
-                _gamePauseCanvas.SetActive(true);
-                Time.timeScale = 0f;
-                activePause = !activePause;
-            }
-            else
-            {
-                _gamePauseCanvas.SetActive(false);
-                Time.timeScale = 1f;
-                activePause = !activePause;
-            } 
+        {
+            _gamePauseCanvas.SetActive(true);
+            Time.timeScale = 0f;
+            activePause = !activePause;
+        }
+        else
+        {
+            _gamePauseCanvas.SetActive(false);
+            Time.timeScale = 1f;
+            activePause = !activePause;
+        }
     }
     public void ExitToDesktop()
     {
@@ -44,7 +49,23 @@ public class GameManager : MonoBehaviour
     }
     public void ExitToMainMenu()
     {
-        
+
+    }
+    // public void ChangedVolume(){
+    //      float AudioVolume = _gamePauseCanvas.GetComponent<Scrollbar>().value;
+    //      _audioSource.GetComponent<AudioSource>().volume = AudioVolume;
+    // }
+    public void ToggleVolume() {
+        if (activeAudio)
+        {
+            _audioSource.GetComponent<AudioSource>().Pause();
+            activeAudio = !activeAudio;
+        }
+        else
+        { 
+            _audioSource.GetComponent<AudioSource>().UnPause();
+            activeAudio = !activeAudio;
+        }
     }
     public void Update()
     {
@@ -53,4 +74,5 @@ public class GameManager : MonoBehaviour
             PauseGame();
         }
     }
+    
 }
