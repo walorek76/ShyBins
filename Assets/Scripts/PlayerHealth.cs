e0ac8a584ec6272af1320723993885c9e9da960a
walorek76 <karollehrich@gmail.com>
2025-10-21 22:51:46 +0200

    add red sprite

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,26 +6,44 @@ public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 5;
     public int currentHealth;
-    
-    
-    
+
+    private SpriteRenderer spriteRenderer;
+    private Color originalColor;
+    private float timer = 0f;
+    private bool isDamaged = false;
+
     void Start()
     {
-        currentHealth = maxHealth;    
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        currentHealth = maxHealth;
+        originalColor = spriteRenderer.color; 
     }
 
-    
     void Update()
     {
         
-    }
-    public void TakeDamage(int amount)
-    {  
-            currentHealth -= amount;
-            if (currentHealth <= 0)
+        if (isDamaged)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= 1f)
             {
-                
-                Destroy(gameObject);
+                spriteRenderer.color = originalColor;
+                timer = 0f;
+                isDamaged = false; 
             }
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        spriteRenderer.color = Color.red;
+        isDamaged = true; 
+
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
